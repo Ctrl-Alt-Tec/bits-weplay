0bdc759bbe4401ca52f479719e67e8e3b7a67537
--replace-all <--replace-all>
2020-01-03 14:40:57 -0600

    Updates testing

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Heroku App Backend using Node.js
+var GoogleSpreadsheet = require('google-spreadsheet');
+//var creds = require('./client_secret.json')
+
+var bits_db = new GoogleSpreadsheet("1kALw2sXhQEohAyW_zL6TMpDLpbHbKvw58evZasUxpI8")
+
+
+bits_db.useServiceAccountAuth(creds, function(err){
+
+    bits_db.getRows(1, function(err, rows){
+        console.log(rows)
+    })
+})
+
 

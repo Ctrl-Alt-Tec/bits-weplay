940cabdc638cd1b52672e2d1aa3a1d15286ba546
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-09-14 18:15:05 -0500

    Created bits for WePlay

--- settings.js
+++ settings.js
@@ -1,17 +1,17 @@
 export default {
     columnNames: {
         member: {
-            name: member => `${member['Nombre(s)']} ${member['Apellido Paterno']} ${member['Apellido Materno']}` || 'Unasigned',
-            id: member => member['Matrícula'],
+            name: member => member['NOMBRE DEL INTEGRANTE'] || 'Unasigned',
+            id: member => member['MATRÍCULA'],
             score: member =>  member?._participations?.total || 1,
-            participations: member => member?._participations.participations
+            participations: member => member?._participations?.participations
         },
         participation: {
-            date: participation => participation['Fecha'], 
-            category: participation => participation['Categoría'], 
-            description: participation => participation['Descripción'], 
-            score: participation => participation['Bits']
+            date: participation => participation['FECHA'], 
+            category: description => 'Puntos por participación', 
+            description: participation => participation['CONCEPTO'], 
+            score: participation => participation['PUNTUACIÓN']
         }
     },
-    baseURL: 'http://ctrl-api.hackclub.com/api/participation?org=ctrl-alt-tec'
+    baseURL: 'http://ctrl-api.hackclub.com/api/participation?org=weplay'
 }
